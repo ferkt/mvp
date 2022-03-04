c3a41d85c1afc1836583aa16cc8614d6d15071a9
Fernando Coelho <fernandocoelho9000@gamil.com>
2022-03-03 23:47:32 -0300

    resolvendo os sairP

--- main/pacient/diagPac.php
+++ main/pacient/diagPac.php
@@ -147,7 +147,7 @@
 				 <a class="dropdown-item" href="#"><i class="ti-wallet text-muted me-2"></i>Minha carteira</a>
 				 <a class="dropdown-item" href="#"><i class="ti-settings text-muted me-2"></i> Configurações</a>
 				 <div class="dropdown-divider"></div>
-				 <a class="dropdown-item" href="../../php/sair.php"><i class="ti-lock text-muted me-2"></i> Sair</a>
+				 <a class="dropdown-item" href="../../php/sairP.php"><i class="ti-lock text-muted me-2"></i> Sair</a>
               </li>
             </ul>
           </li>	
