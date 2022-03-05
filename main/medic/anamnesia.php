eb60853539fce7ce5f8582eaf3639f7654dc87dc
Ever-5 <everon3210@gmail.com>
2022-03-04 21:18:35 -0300

    BetaEver

--- main/medic/anamnesia.php
+++ main/medic/anamnesia.php
@@ -534,7 +534,7 @@
 				<div class="d-flex align-items-end justify-content-between" style="margin-top: 12px;">
 					<button type="submit" class="waves-effect waves-light btn btn-primary mb-6" _msthash="3547583" _msttexthash="133497"><i class="fa fa-fw fa-save"></i> Salvar</button>
 
-					<a type="button" href="infopac.php?mess=cancel"class="waves-effect waves-light btn btn-danger mb-6" _msthash="3547583" _msttexthash="133497"><i class="fa fa-trash" aria-hidden="true"></i> Excluir</a>
+					<a type="button" href="infopac.php?mess=cancel&id_pac=<?php echo $_GET['id_pac']?>" class="waves-effect waves-light btn btn-danger mb-6" _msthash="3547583" _msttexthash="133497"><i class="fa fa-trash" aria-hidden="true"></i> Excluir</a>
 				</div>
 				
 			</div>
